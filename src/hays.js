9f8f730e481d5e35b87c9a5cc9f2cd28ab9813c2
DankTrain <megablaziken092505@gmail.com>
2019-09-14 21:19:42 -0400

    9/14/19 - 9:19PM
    Add hays gif and update info command

---
+++ src/hays.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args, cube) => {
+	message.channel.send({
+		files: [{
+			attachment: "./src/gifs/hays.gif",
+			name: "hays.gif"
+		}]
+	});
+};
+module.exports.config = { name: "hays", aliases: [] };
