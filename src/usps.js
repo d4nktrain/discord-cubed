624cd3a019d17620f2fdaf8a228d84dab592eb14
Ethan Hardy <ethanmgentz@gmail.com>
2019-09-14 22:40:27 -0400

    9/14/19 - 10:40PM
    Add usps (trash) command
    Fix help showing bad oh list

---
+++ src/usps.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args, cube) => {
+	message.channel.send({
+		files: [{
+			attachment: "./src/gifs/usps.gif",
+			name: "usps.gif"
+		}]
+	});
+	return message.reply("This is what happens to your cubes when you ship with USPS")
+};
+module.exports.config = { name: "usps", aliases: [] };
