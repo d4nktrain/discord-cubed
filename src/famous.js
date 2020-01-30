78e09c39ac19076517a63f7abbf6b966137b3e7b
Ethan Hardy <45316161+DiscordGod@users.noreply.github.com>
2020-01-30 16:48:02 -0500

    1/30/2020 - 4:48PM
    Fix aliases in submit.js
    Fix context menu in famous.js

--- src/famous.js
+++ src/famous.js
@@ -3,8 +3,8 @@ module.exports.run = async (bot, message, args, cube) => {
 	if(!args[0] || args[0] == "help") {
         return message.channel.send(new Discord.RichEmbed()
         .setTitle("Get famous scrambles").setColor("RANDOM")
-        .setDescription("Usage: \`s!famous <person>\`")
-        .addField("Options", "jay"))}
+        .setDescription("Usage: \`s!famous <solve>\`")
+        .addField("Options", "jay, 3x3wr"))}
     if(args[0] == "jay") {
         message.channel.send("Scramble: L2 U L2 D F2 D' F2 R2 D L2 B R2 F' L2 D' L B' U2")
         return message.channel.send("Reconstruction:\nx' z2  // Inspection\nF D R2' F' R // X-Cross\ny U' R' U R // Pair 2\nL U L' U L U L' // Pair 3\ny' R' U R // Pair 4\nR' U' R' F R F' U R // OLL \nU'// Attempted AUF\n[massive fumbles equivalent to z'] // rip\nL2// PLL skip")
