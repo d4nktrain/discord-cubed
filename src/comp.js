532b7c97c563a30d7e6c5c8837e529c88889cad8
Ethan Hardy <45316161+DiscordGod@users.noreply.github.com>
2020-02-02 12:57:45 -0500

    Update comp.js

--- src/comp.js
+++ src/comp.js
@@ -98,10 +98,10 @@ module.exports.run = async (bot, message, args, cube) => {
 	}
 	msgArr.forEach(event => {
 		setTimeout(() => {
-			message.channel.send(event);setTimeout(() => message.channel.send("**Latest Update:**\n•Added 17 more egg gifs\n\n**If you enjoy the use of the bot, please consider donating. All donations go only to keeping the bot online.**\nJust $5 keeps the bot up for a whole month.\nhttps://paypal.me/HellaBread17Cent"), 12500);
+			message.channel.send(event);
 		}, 1500);
 	});
 	setTimeout(() => message.channel.send("Competition scrambles complete.").then(msg => msg.delete(10000)), 12500);
-	
+	setTimeout(() => message.channel.send("**Latest Update:**\n•Added 17 more egg gifs\n\n**If you enjoy the use of the bot, please consider donating. All donations go only to keeping the bot online.**\nJust $5 keeps the bot up for a whole month.\nhttps://paypal.me/HellaBread17Cent"), 12500);
 };
 module.exports.config = { name: "comp", aliases: ["comps", "compscrambles"] };
