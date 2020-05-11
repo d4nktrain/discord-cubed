896572311fb2d5dd1390a1579e617e60fc86e78e
Ethan Hardy <45316161+DiscordGod@users.noreply.github.com>
2020-05-10 22:40:15 -0400

    fix mega scrams

--- src/megaminx.js
+++ src/megaminx.js
@@ -1,20 +1,12 @@
+import {util_scramble} from "./ilovecstimer/utilscramble";
+
 module.exports.run = async (bot, message, args, cube) => {
-	let msgArr = [];
 	let scrambles = parseInt(args[0]);
 	scrambles = scrambles ? scrambles > 6 ? 6 : scrambles < 0 ? 1 : scrambles : 1;
-
-	for(var i = 0; i < scrambles; i++) {
-		for(var i = 1, scramble = []; i < 78; i++) {
-			if(i !== 1 && i % 11 === 0) {
-				scramble[i - 2][2] === "-" ? scramble.push("U\'\n") : scramble.push("U\n");
-			} else if(i === 1 || scramble[i - 2][0] === "D" || scramble[i - 2][0] === "U") {
-				scramble.push(`R${Math.random() < 0.5 ? "++" : "--"}`);
-			} else {
-				scramble.push(`D${Math.random() < 0.5 ? "++" : "--"}`);
-			}
-		}
-		msgArr.push(`\`\`\`\n${scramble.join(" ").replace(/U\n R/g, "U\nR").replace(/U'\n R/g, "U\'\nR")}\n\`\`\``);
+	let scramble = []
+	for(let i = 0; i < scrambles; i++) {
+		scramble[i] = (i+1) + ". " + "\n```" + util_scramble.getMegaminxWCAScramble(70) + "```"
 	}
-	for(var i = 0; i < msgArr.length; i++) {msgArr[i] = (i+1) + ". " + msgArr[i]}; return message.channel.send(msgArr.join("\n\n"));
+	return message.channel.send(scramble.join(""));
 };
 module.exports.config = { name: "megaminx", aliases: ["mega", "minx", "mm", "m"] };
