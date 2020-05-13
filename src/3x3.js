f04c06f7ede15434c44b07f4b7aedee038f47fd0
Ethan Hardy <45316161+iseexsd@users.noreply.github.com>
2020-05-13 00:27:32 -0400

    add the ability to show scrambles visually

--- src/3x3.js
+++ src/3x3.js
@@ -1,9 +1,57 @@
 import {scramble_333} from "./cstimerlib/scramble_333_edit";
+import {getEmojiArray} from "./jaapschlib/genScramble3";
+import {fourByFour, threeByThree} from "./jaapschlib/genVisualScramble";
+import {scramble_444} from "./cstimerlib/scramble_444";
 
-module.exports.run = async (bot, message, args, cube) => {
-	let scramble = []
-	let scrambles = parseInt(args[0])
-	scrambles = scrambles ? scrambles > 12 ? 12 : scrambles < 0 ? 1 : scrambles : 1
-	for(var i = 0; i < scrambles; i++) {scramble[i] = (i+1) + ". " + scramble_333.getRandomScramble()}; return message.channel.send(scramble.join("\n\n"));
+var fs = require("fs")
+
+function jaapschSeq(scramble) {
+    scramble = scramble.split(" ")
+    for(var i = 0; i < scramble.length; i++) {
+        if(scramble[i] === "D") scramble[i] = 0
+        if(scramble[i] === "D2") scramble[i] = 1
+        if(scramble[i] === "D'") scramble[i] = 2
+        if(scramble[i] === "L") scramble[i] = 4
+        if(scramble[i] === "L2") scramble[i] = 5
+        if(scramble[i] === "L'") scramble[i] = 6
+        if(scramble[i] === "B") scramble[i] = 8
+        if(scramble[i] === "B2") scramble[i] = 9
+        if(scramble[i] === "B'") scramble[i] = 10
+        if(scramble[i] === "U") scramble[i] = 12
+        if(scramble[i] === "U2") scramble[i] = 13
+        if(scramble[i] === "U'") scramble[i] = 14
+        if(scramble[i] === "R") scramble[i] = 16
+        if(scramble[i] === "R2") scramble[i] = 17
+        if(scramble[i] === "R'") scramble[i] = 18
+        if(scramble[i] === "F") scramble[i] = 20
+        if(scramble[i] === "F2") scramble[i] = 21
+        if(scramble[i] === "F'") scramble[i] = 22
+    }
+
+    scramble.pop()
+    scramble.push("0")
+
+    return scramble
+}
+
+module.exports.run = async (bot, message, count) => {
+    let scramble = scramble_333.getRandomScramble()
+
+    var indent = "                   "
+    var emojiArray = getEmojiArray(jaapschSeq(scramble))
+    var scrambleImage = threeByThree(indent, emojiArray)
+
+    return message.channel.send(count + ". " + scramble).then((msg) => {
+        msg.react("👀")
+        msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
+            { max: 1, time: 10000 }).then(collected => {
+            if (collected.first().count >= 2) {
+                msg.edit(count + ". " + scramble + scrambleImage)
+            }
+            msg.clearReactions()
+        }).catch(() => {
+            msg.clearReactions()
+        });
+    })
 };
 module.exports.config = { name: "3x3", aliases: ["3x3x3", "3"] }
