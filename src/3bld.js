4570daa9691a20fdab6d1f6e90aaa6935227d9f0
Ethan Hardy <45316161+iseexsd@users.noreply.github.com>
2020-05-16 19:28:46 -0400

    fix problems with 3x3 type scrambles

--- src/3bld.js
+++ src/3bld.js
@@ -17,15 +17,15 @@ module.exports.run = async (bot, message, args, cube) => {
         var rotation3 = randomElement(["Uw", "Uw2", "Uw'"])
         var whatRotation = Math.floor(Math.random()*5)
         if(whatRotation == 0) {
-		    scramble[i] = (i+1) + ". " + scramble[i] + " " + rotation1 + " " + rotation3
+		    scramble[i] = (i+1) + ". " + scramble[i] + rotation1 + " " + rotation3
         } else if(whatRotation == 1) {
-		    scramble[i] = (i+1) + ". " + scramble[i] + " " + rotation2 + " " + rotation3
+		    scramble[i] = (i+1) + ". " + scramble[i] + rotation2 + " " + rotation3
         } else if(whatRotation == 2) {
-		    scramble[i] = (i+1) + ". " + scramble[i] + " " + rotation1
+		    scramble[i] = (i+1) + ". " + scramble[i] + rotation1
         } else if(whatRotation == 3) {
-		    scramble[i] = (i+1) + ". " + scramble[i] + " " + rotation2
+		    scramble[i] = (i+1) + ". " + scramble[i] + rotation2
         } else if(whatRotation == 4) {
-		    scramble[i] = (i+1) + ". " + scramble[i] + " " + rotation3
+		    scramble[i] = (i+1) + ". " + scramble[i] + rotation3
         }
 	}
 	return message.channel.send(scramble.join("\n\n"));
