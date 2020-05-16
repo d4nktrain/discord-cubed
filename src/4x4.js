1e78f63d17872f5ed5b6e2ae6dd030ac5763914c
Ethan Hardy <45316161+iseexsd@users.noreply.github.com>
2020-05-16 18:08:51 -0400

    fixed the visual scrambler converting Fw2 to 21 21 (F2 F2)

--- src/4x4.js
+++ src/4x4.js
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args, cube) => {
 					}).then(async () => {
 						let image = await Jimp.read('./pngs/' + msg.id + '.png')
 						image.crop(1, 1, 161, 121).resize(320, 240, Jimp.RESIZE_NEAREST_NEIGHBOR).write('./pngs/' + msg.id + '.png', () => {
-							msg.channel.send("The visualization process is currently unstable, do not expect it to be right", {
+							msg.channel.send("", {
 								file: './pngs/' + msg.id + '.png'
 							}).then(() => {
 								fs.unlinkSync('./pngs/' + msg.id + '.png')
