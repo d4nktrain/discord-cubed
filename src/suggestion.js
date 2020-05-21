7bbfb1763f6294fb78c613a2ded5af3b462d77aa
Ethan Hardy <ethanmgentz@gmail.com>
2020-05-21 19:07:52 -0400

    add ability to view and set status of suggestions

--- src/suggestion.js
+++ src/suggestion.js
@@ -9,15 +9,48 @@ module.exports.run = async (bot, message, args) => {
 			.setDescription("Usage: \`s!suggest <suggestion>\` or \`s!suggest view\`"))
     }
     if(args[0] === "view") {
-        return message.channel.send("```" + fs.readFileSync(__dirname + '/../suggestions.json') + "```")
+        let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
+
+        let embed = new Discord.RichEmbed().setTitle("Your suggestions")
+        bot.fetchUser(message.author.id).then(user => {
+            for(let i = 0; i < suggestionsArray.length; i++) {
+                if(suggestionsArray[i].person === user.tag) {
+                    if(suggestionsArray[i].status) {
+                        embed.addField(suggestionsArray[i].suggestion, suggestionsArray[i].status)
+                    } else {
+                        embed.addField(suggestionsArray[i].suggestion, "Hasn't been reviewed yet")
+                    }
+                }
+            }
+            return message.channel.send(embed)
+        })
+        return
+    }
+    if((args[0] === "status") && message.author.id === "182620322846081024") {
+        let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
+
+        args.shift()
+        let statusIn = args.join(" ").split("|")[0]
+
+        let suggestion = args.join(" ").split("|")[1]
+
+        for(let i = 0; i < suggestionsArray.length; i++) {
+            if(suggestionsArray[i].suggestion === suggestion) {
+                suggestionsArray[i].status = statusIn
+                fs.writeFileSync(__dirname + '/../suggestions.json', JSON.stringify(suggestionsArray))
+                message.channel.send("Successfully set status!")
+            }
+        }
+        return
     }
     let suggestion = args.join(" ")
-    if(suggestion.indexOf("1x1") != -1) return message.channel.send("no. it could've been funny the first time someone said it, and it still wasn't. stop.")
-    let file = fs.readFileSync(__dirname + '/../suggestions.json')
+    if((suggestion.indexOf("1x1") != -1) || (suggestion.indexOf("0x0") != -1) || (suggestion.indexOf("your idea") != -1)) return message.channel.send("no. it could've been funny the first time someone said it, and it still wasn't. stop.")
+    let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
     bot.fetchUser(message.author.id).then(user => {
-        fs.writeFileSync(__dirname + '/../suggestions.json', file + "\n" + suggestion + " by: " + user.tag)
+        suggestionsArray.push({"suggestion": suggestion, "person": user.tag})
+        fs.writeFileSync(__dirname + '/../suggestions.json', JSON.stringify(suggestionsArray))
     })
     console.log("Wrote " + suggestion + " as a suggestion")
-    return message.channel.send("Wrote " + suggestion + " as a suggestion!")
+    return message.channel.send("Wrote " + suggestion + " as a suggestion! If you want to see the status of your suggestion, do " + message.content.split(" ")[0] + " view")
 }
 module.exports.config = { name: "suggest", aliases: ["suggestion, suggestions"] }
