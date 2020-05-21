63f4ca17d241ebe2522e751db2e164a21abdbbf6
iseexsd <megablaziken092505@gmail.com>
2020-05-21 12:02:14 -0400

    add ability to view suggestions

--- src/suggestion.js
+++ src/suggestion.js
@@ -4,9 +4,12 @@ const fs = require('fs')
 module.exports.run = async (bot, message, args) => {
     if(!args[0] || args[0] == "help") {
 		return message.channel.send(new Discord.RichEmbed()
-			.setTitle("Send suggestions to danktrain")
+			.setTitle("Send suggestions to the dev (danktrain#0001)")
 			.setColor("RANDOM")
-			.setDescription("Usage: \`s!suggest <suggestion>\`"))
+			.setDescription("Usage: \`s!suggest <suggestion>\` or \`s!suggest view\`"))
+    }
+    if(args[0] === "view") {
+        return message.channel.send("```" + fs.readFileSync(__dirname + '/../suggestions.json') + "```")
     }
     let suggestion = args.join(" ")
     if(suggestion.indexOf("1x1") != -1) return message.channel.send("no. it could've been funny the first time someone said it, and it still wasn't. stop.")
