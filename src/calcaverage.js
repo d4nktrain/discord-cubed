c285eda46d3e8790ab143d2e39e98249cbb6f8a9
Ethan Hardy <ethanmgentz@gmail.com>
2020-05-21 21:07:31 -0400

    wca compliant avg calculation and it actually works :P

--- src/calcaverage.js
+++ src/calcaverage.js
@@ -1,10 +1,19 @@
 module.exports.run = async (bot, message, args, cube) => {
-    let times = 0
 
-    for(let i = 0; i < args.length; i++) {
-        times = times + parseInt(args[i])
+    let sum = 0
+    let time = 0
+
+    for (var i = 0; i < args.length; i++) {
+        sum += Number(args[i])
     }
 
-    return message.channel.send(times/args.length)
+    time = String(sum/args.length)
+
+    if(time.split("").length > 5) {
+        time = time.split("")
+        time = time[0] + time[1] + time[2] + time[3] + time[4]
+    }
+
+    return message.channel.send(time)
 };
 module.exports.config = { name: "avg", aliases: ["average", "calcaverage", "calcavg"] };
