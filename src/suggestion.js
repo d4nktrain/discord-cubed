8bb78673b3e18d752f5142c53fef3bd970bc0e22
iseexsd <megablaziken092505@gmail.com>
2020-05-22 23:16:13 -0400

    add a viewer for suggestions

--- src/suggestion.js
+++ src/suggestion.js
@@ -8,9 +8,11 @@ module.exports.run = async (bot, message, args) => {
 			.setColor("RANDOM")
 			.setDescription("Usage: \`s!suggest <suggestion>\`, \`s!suggest view\`, or \`s!suggest view <tag>\`"))
     }
+
+    let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
+
     if(args[0] === "view") {
         args.shift()
-        let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
 
         let embed = new Discord.RichEmbed()
         if(args.join(" ")) {
@@ -42,9 +44,25 @@ module.exports.run = async (bot, message, args) => {
         }
         return
     }
+    if((args[0] === "viewall") && message.author.id === "182620322846081024") {
+        var i,j,temparray,chunk = 25;
+        for (i=0,j=suggestionsArray.length; i<j; i+=chunk) {
+            let embed = new Discord.RichEmbed();
+            temparray = suggestionsArray.slice(i,i+chunk);
+            for(let k = 0; k < temparray.length; k++) {
+                if (!(temparray[k].status === "done!")) {
+                    if (temparray[k].status) {
+                        if (!temparray[k].status.includes("denied")) embed.addField(temparray[k].person, temparray[k].suggestion)
+                    } else {
+                        embed.addField(temparray[k].person, temparray[k].suggestion)
+                    }
+                }
+            }
+            message.channel.send(embed)
+        }
+        return
+    }
     if((args[0] === "status") && message.author.id === "182620322846081024") {
-        let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
-
         args.shift()
         let statusIn = args.join(" ").split("|")[0]
 
@@ -61,7 +79,6 @@ module.exports.run = async (bot, message, args) => {
     }
     let suggestion = args.join(" ")
     if((suggestion.indexOf("1x1") != -1) || (suggestion.indexOf("0x0") != -1) || (suggestion.indexOf("your idea") != -1)) return message.channel.send("no. it could've been funny the first time someone said it, and it still wasn't. stop.")
-    let suggestionsArray = JSON.parse(fs.readFileSync(__dirname + '/../suggestions.json').toString())
     bot.fetchUser(message.author.id).then(user => {
         suggestionsArray.push({"suggestion": suggestion, "person": user.tag})
         fs.writeFileSync(__dirname + '/../suggestions.json', JSON.stringify(suggestionsArray))
