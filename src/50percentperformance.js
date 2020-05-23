34686289b0cd31352de5a958488609766b858807
iseexsd <megablaziken092505@gmail.com>
2020-05-23 01:02:17 -0400

    lol inside joke

---
+++ src/50percentperformance.js
@@ -0,0 +1,4 @@
+module.exports.run = async (bot, message) => {
+    return message.channel.send("With Intel 10th gen, you pay 50 bucks more for 5% more performance :)")
+};
+module.exports.config = { name: "50_bucks_for_+5%_performance_compared_to_AMD_counterparts", aliases: [] };
