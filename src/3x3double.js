96affda3a4747dbf261802ac39b91f96b6ea206e
iseexsd <megablaziken092505@gmail.com>
2020-06-09 23:09:43 -0400

    stuff i can't be bothered to make a message

--- src/3x3double.js
+++ src/3x3double.js
@@ -1,4 +1,4 @@
 module.exports.run = async (bot, message, args, cube, scrambleImage) => {
-    return message.channel.send("This command is deprecated, please use (prefix)3 double, 3x3x3 double, or 3x3 double!")
+    return message.channel.send("This command is deprecated, please use s!3 double, 3x3x3 double, or 3x3 double!")
 };
 module.exports.config = { name: "3x3double", aliases: ["3x3d", "3d"] }
