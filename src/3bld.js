d9585ed8eb29ef66732e9ce36f70cee0bc92ecf5
Ethan Hardy <ethanmgentz@gmail.com>
2020-06-09 23:09:43 -0400

    stuff i can't be bothered to make a message

--- src/3bld.js
+++ src/3bld.js
@@ -1,4 +1,4 @@
 module.exports.run = async (bot, message, args, cube) => {
-    return message.channel.send("This command is deprecated, please use (prefix)3 bld, 3x3x3 bld, or 3x3 bld!")
+    return message.channel.send("This command is deprecated, please use s!3 bld, 3x3x3 bld, or 3x3 bld!")
 }
 module.exports.config = { name: "3bld", aliases: ["3-bld", "3-BLD", "3b"] }
--- src/6x6.js
+++ src/6x6.js
@@ -7,23 +7,7 @@ module.exports.run = async (bot, message, args, scrambleImage) => {
 	for(let i = 0; i < scrambles; i++) {
 		let scramble = [`${i+1}. `, megaScrambler.get666WCAScramble(80)]
 
-		message.channel.send(scramble.join("")).then((msg) => {
-			msg.react("👀")
-			msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
-				{max: 1, time: 15000}).then(async collected => {
-				if (collected.first().count >= 2) {
-					var imageBuffer = await scrambleImage.genImage("666", scramble[1], "default")
-					msg.channel.send(i+1 + ".", {
-						file: imageBuffer
-					}).then((image) => {
-						image.delete(300000*(i+1))
-					})
-				}
-				msg.clearReactions()
-			}).catch(() => {
-				msg.clearReactions()
-			});
-		})
+		message.channel.send(scramble.join(""))
 	}
 };
 module.exports.config = { name: "6x6", aliases: ["6x6x6", "6"] }
