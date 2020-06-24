8b597cc937d5c93e4868818f7355896782f63e63
Ethan Hardy <ethanmgentz@gmail.com>
2020-06-23 22:18:23 -0400

    add megaminx carrot notation

--- src/megaminx.js
+++ src/megaminx.js
@@ -9,28 +9,90 @@ function replaceAll(str, term, replacement) {
 }
 
 module.exports.run = async (bot, message, args, cube, scrambleImage) => {
-	let scrambles = parseInt(args[0]);
-	scrambles = scrambles ? scrambles > 6 ? 6 : scrambles < 0 ? 1 : scrambles : 1;
-	for(let i = 0; i < scrambles; i++) {
-		let scramble = ["", replaceAll(util_scramble.getMegaminxWCAScramble(70), "  ", "")]
+	let scrambles = parseInt(args[0])
+	if(isNaN(scrambles) && args[0]) {
+		if(args[0] === "carrot") {
+			let scrambles = parseInt(args[1]);
+			scrambles = scrambles ? scrambles > 6 ? 6 : scrambles < 0 ? 1 : scrambles : 1;
+			for(let i = 0; i < scrambles; i++) {
+				let scramble = ["", replaceAll(util_scramble.getMegaminxCarrotScramble(70), "  ", "")]
 
-		message.channel.send((i+1) + ". " + "```" + scramble[1] + "```").then((msg) => {
-			msg.react("👀")
-			msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
-				{ max: 1, time: 15000 }).then(async collected => {
-				if (collected.first().count >= 2) {
-					var imageBuffer = await scrambleImage.genImage("megaminx", scramble[1], "default")
-					msg.channel.send(i+1 + ".", {
-						file: imageBuffer
-					}).then((image) => {
-						image.delete(300000*(i+1))
-					})
-				}
-				msg.clearReactions()
-			}).catch(() => {
-				msg.clearReactions()
-			});
-		})
+				message.channel.send((i+1) + ". " + "```" + scramble[1] + "```").then((msg) => {
+					msg.react("👀")
+					msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
+						{ max: 1, time: 15000 }).then(async collected => {
+						if (collected.first().count >= 2) {
+							let toNormal = scramble[1].split(" ")
+							for(let i = 0; i < toNormal.length; i++) {
+								if(toNormal[i].includes("+") || toNormal[i].includes("-")) {
+									let newMoves = toNormal[i].split("")
+									for(let j = 0; j < newMoves.length; j++) {
+										if(j === 0) newMoves[j] = "R" + newMoves[j] + newMoves[j]
+											else if(j === 1) newMoves[j] = "D" + newMoves[j] + newMoves[j]
+									}
+									toNormal[i] = newMoves.join(" ")
+								}
+							}
+							var imageBuffer = await scrambleImage.genImage("megaminx", toNormal.join(" "), "default")
+							msg.channel.send(i+1 + ".", {
+								file: imageBuffer
+							}).then((image) => {
+								image.delete(300000*(i+1))
+							})
+						}
+						msg.clearReactions()
+					}).catch(() => {
+						msg.clearReactions()
+					});
+				})
+			}
+		}
+	} else {
+		scrambles = scrambles ? scrambles > 6 ? 6 : scrambles < 0 ? 1 : scrambles : 1;
+		for (let i = 0; i < scrambles; i++) {
+			let scramble = ["", replaceAll(util_scramble.getMegaminxWCAScramble(70), "  ", "")]
+
+			message.channel.send((i + 1) + ". " + "```" + scramble[1] + "```").then((msg) => {
+				msg.react("👀")
+				msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
+					{max: 1, time: 15000}).then(async collected => {
+					if (collected.first().count >= 2) {
+						var imageBuffer = await scrambleImage.genImage("megaminx", scramble[1], "default")
+						msg.channel.send(i + 1 + ".", {
+							file: imageBuffer
+						}).then((image) => {
+							image.delete(300000 * (i + 1))
+						})
+					}
+					msg.clearReactions()
+				}).catch(() => {
+					msg.clearReactions()
+				});
+			})
+		}
 	}
+	// let scrambles = parseInt(args[0]);
+	// scrambles = scrambles ? scrambles > 6 ? 6 : scrambles < 0 ? 1 : scrambles : 1;
+	// for(let i = 0; i < scrambles; i++) {
+	// 	let scramble = ["", replaceAll(util_scramble.getMegaminxWCAScramble(70), "  ", "")]
+	//
+	// 	message.channel.send((i+1) + ". " + "```" + scramble[1] + "```").then((msg) => {
+	// 		msg.react("👀")
+	// 		msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👀'),
+	// 			{ max: 1, time: 15000 }).then(async collected => {
+	// 			if (collected.first().count >= 2) {
+	// 				var imageBuffer = await scrambleImage.genImage("megaminx", scramble[1], "default")
+	// 				msg.channel.send(i+1 + ".", {
+	// 					file: imageBuffer
+	// 				}).then((image) => {
+	// 					image.delete(300000*(i+1))
+	// 				})
+	// 			}
+	// 			msg.clearReactions()
+	// 		}).catch(() => {
+	// 			msg.clearReactions()
+	// 		});
+	// 	})
+	// }
 };
 module.exports.config = { name: "megaminx", aliases: ["mega", "minx", "mm", "m"] };
